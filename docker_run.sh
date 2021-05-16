41f8c361b818e365d060acca0351daca5637c07f
Krithika16 <krithika.balaji18@gmail.com>
2021-05-16 16:22:33 +0530

    change docker image and container names

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -it --gpus=all -u $(id -u):$(id -g) --ipc=host --name augcon -v /home/krithika/DDH_Project/DDH_Project/:/home/krithika/DDH_Project/DDH_Project/ tf_aug
+docker run -it --gpus=all -u $(id -u):$(id -g) --ipc=host --name dock_container1 -v /home/krithika/DDH_Project/DDH_Project/:/home/krithika/DDH_Project/DDH_Project/ dock_image1
