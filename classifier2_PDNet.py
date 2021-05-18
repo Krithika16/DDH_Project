4a40153610d54b0c6741803693b3841fb0cfc181
Krithika16 <krithika.balaji18@gmail.com>
2021-05-18 16:14:11 +0530

    Redo training of PDNet 1 to get model saved

--- classifier2_PDNet.py
+++ classifier2_PDNet.py
@@ -27,7 +27,7 @@ import tensorflow.keras.backend as K
 def Classify2_PDNet():    #again specifiy the function to be 2 and hence refere to the alpha angle
 
   # Training Parameters
-  epochs = 3 #going through the dataset 50 times
+  epochs = 40 #going through the dataset 50 times
   batch_size = 32 #Number of samples passed through CNN at one time for training data
   test_batch_size = 16 #batch size for testing data
   val_batch_size = 16 #batch size for validation data
