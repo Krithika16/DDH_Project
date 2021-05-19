ad37759d3076773fd16ac5d72e2cfb107286a5ac
Krithika16 <krithika.balaji18@gmail.com>
2021-05-19 18:42:41 +0530

    Training resnet uncr: 0.001 lr, 16 bs, 40 ep

--- classifier2_uncroppedscansonly.py
+++ classifier2_uncroppedscansonly.py
@@ -21,7 +21,7 @@ import tensorflow.keras.backend as K
 def Classify2_uncroppedScans():    #again specifiy the function to be 2 and hence refere to the alpha angle
 
   # Training Parameters
-  epochs = 50
+  epochs = 40
   batch_size = 16
   test_batch_size = 8
   val_batch_size = 8
